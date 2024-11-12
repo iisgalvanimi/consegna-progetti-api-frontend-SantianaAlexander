4b05c171b6408573067d5b6d63148f12edf59cff
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-11-12 10:36:57 +0000

    FIX DELLA PAGINA FRUIT

--- routing-test-app/src/app/fruit-component/fruit-component.component.ts
+++ routing-test-app/src/app/fruit-component/fruit-component.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./fruit-component.component.css']
 })
 export class FruitComponentComponent {
-
+  fruits = [
+    { name: 'mango', emoji: '🥭' },
+    { name: 'banana', emoji: '🍌' },
+    { name: 'apple', emoji: '🍎' },
+    { name: 'cherry', emoji: '🍒' },
+    { name: 'grapes', emoji: '🍇' },
+    { name: 'watermelon', emoji: '🍉' },
+    { name: 'pineapple', emoji: '🍍' },
+    { name: 'orange', emoji: '🍊' },
+    { name: 'pear', emoji: '🍐' },
+    { name: 'strawberry', emoji: '🍓' },
+    { name: 'lemon', emoji: '🍋' },
+    { name: 'peach', emoji: '🍑' },
+    { name: 'plum', emoji: '🍑' },
+    { name: 'apricot', emoji: '🍑' },
+    { name: 'blueberry', emoji: '🫐' },
+    { name: 'kiwi', emoji: '🥝' },
+    { name: 'melon', emoji: '🍈' },
+    { name: 'coconut', emoji: '🥥' },
+    { name: 'papaya', emoji: '🍍' },
+    { name: 'pomegranate', emoji: '🍎' },
+    { name: 'fig', emoji: '🍏' },
+    { name: 'avocado', emoji: '🥑' },
+    { name: 'clementine', emoji: '🍊' },
+    { name: 'pear', emoji: '🍐' },
+    { name: 'date', emoji: '🌴' },
+    { name: 'blackberry', emoji: '🍇' },
+    { name: 'raspberry', emoji: '🍇' },
+    { name: 'persimmon', emoji: '🍑' },
+    { name: 'tangerine', emoji: '🍊' },
+    { name: 'dragon fruit', emoji: '🍍' },
+    { name: 'apples', emoji: '🍏' },
+    { name: 'lychee', emoji: '🍍' },
+    { name: 'pear', emoji: '🍐' },
+    { name: 'grapefruit', emoji: '🍊' }
+  ];
 }
