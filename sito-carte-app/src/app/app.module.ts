55bc7ae2a9b0631666a2c3bbea50a9e37a361130
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-11-15 10:51:20 +0000

    CREAZIONE COMPONENTI CARTE

--- sito-carte-app/src/app/app.module.ts
+++ sito-carte-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Carta1Component } from './carta1/carta1.component';
+import { Carta2Component } from './carta2/carta2.component';
+import { Carta3Component } from './carta3/carta3.component';
+import { Carta4Component } from './carta4/carta4.component';
+import { Carta5Component } from './carta5/carta5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Carta1Component,
+    Carta2Component,
+    Carta3Component,
+    Carta4Component,
+    Carta5Component
   ],
   imports: [
     BrowserModule,
