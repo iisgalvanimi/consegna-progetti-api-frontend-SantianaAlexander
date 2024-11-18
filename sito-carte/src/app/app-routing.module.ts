c3fb32c22d7502f2603378fb9781713aed2e391c
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-11-18 13:37:10 +0000

    ROUTING FATTO

--- sito-carte/src/app/app-routing.module.ts
+++ sito-carte/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { Carta1Component } from './carta1/carta1.component';
 import { Carta2Component } from './carta2/carta2.component';
 import { Carta3Component } from './carta3/carta3.component';
 import { Carta4Component } from './carta4/carta4.component';
 import { Carta5Component } from './carta5/carta5.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
   {path:'carta1', component: Carta1Component},
@@ -12,11 +14,13 @@ const routes: Routes = [
   {path:'carta3', component: Carta3Component},
   {path:'carta4', component: Carta4Component},
   {path:'carta5', component: Carta5Component},
-  {path: '', redirectTo: '/carta1', pathMatch: 'full'},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    BrowserModule, HttpClientModule
+  ],
   exports: [RouterModule]
+ 
 })
 export class AppRoutingModule { }
--- sito-carte/src/app/carta1/carta1.component.ts
+++ sito-carte/src/app/carta1/carta1.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-carta1',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./carta1.component.css']
 })
 export class Carta1Component {
+  
+  data!: Object;
+   loading!: boolean;
+   o! :Observable<any>;
+   constructor(public http: HttpClient) {}
+   
+   makeRequest(): void {
+     console.log("here");
+     this.loading = true;
+     this.o = this.http.get<any>('https://pokeapi.co/api/v2/pokemon/ditto');
+     this.o.subscribe(this.getData);
+   }
+   getData = (d : any) =>
+   {
+     this.data = new Object(d);
+     this.loading = false;
+   }
+
 
 }
