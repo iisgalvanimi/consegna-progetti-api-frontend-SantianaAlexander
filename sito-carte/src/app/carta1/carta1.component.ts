f67332e17d4949e621b846fb1d5dcd0802090e75
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-11-20 08:47:43 +0000

    DITTO

--- sito-carte/src/app/carta1/carta1.component.ts
+++ sito-carte/src/app/carta1/carta1.component.ts
@@ -2,17 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
+interface PokemonData {
+  name: string;
+  
+}
+
 @Component({
   selector: 'app-carta1',
   templateUrl: './carta1.component.html',
   styleUrls: ['./carta1.component.css']
 })
-export class Carta1Component {
+export class Carta1Component implements OnInit {
   
-  data!: Object;
+  data!: PokemonData;
    loading!: boolean;
    o! :Observable<any>;
    constructor(public http: HttpClient) {}
+
+   ngOnInit(): void {
+    this.makeRequest();  // Effettua la richiesta subito dopo che il componente è stato caricato
+  }
    
    makeRequest(): void {
      console.log("here");
@@ -20,11 +29,10 @@ export class Carta1Component {
      this.o = this.http.get<any>('https://pokeapi.co/api/v2/pokemon/ditto');
      this.o.subscribe(this.getData);
    }
-   getData = (d : any) =>
-   {
-     this.data = new Object(d);
-     this.loading = false;
-   }
+   getData = (d: PokemonData) => {
+    this.data = d;
+    this.loading = false;
+  }
 
 
 }
